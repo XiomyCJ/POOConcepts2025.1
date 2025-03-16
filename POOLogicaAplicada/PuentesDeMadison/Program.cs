7a3aa538f431bf756201daa821ff7305289956bc
Xiomara C.J <xiomy1992-@hotmail.com>
2025-03-15 19:03:10 -0500

    Cosechando

---
+++ POOLogicaAplicada/PuentesDeMadison/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PuentesMadisonApp
+{
+    using PuentesMadison;
+
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.Write("Ingrese el puente: ");
+                string? input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    continue;
+                }
+
+                List<char> estructura = new List<char>(input.ToCharArray());
+
+                try
+                {
+                    var puente = new Puente(estructura);
+                    Console.WriteLine("VALIDO");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ POOLogicaAplicada/PuentesDeMadison/PuentesDeMadison.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PuentesMadison
+{
+    public class Puente
+    {
+        private List<char> _estructura;
+
+        public List<char> Estructura
+        {
+            get => _estructura;
+            set => _estructura = ValidarEstructura(value);
+        }
+
+        private List<char> ValidarEstructura(List<char> estructura)
+        {
+            if (!EsSimetrico(estructura))
+            {
+                throw new ArgumentException("INVALIDO");
+            }
+            if (!BasesEnExtremos(estructura))
+            {
+                throw new ArgumentException("INVALIDO");
+            }
+            return estructura;
+        }
+
+        private bool EsSimetrico(List<char> estructura)
+        {
+            int n = estructura.Count;
+            for (int i = 0; i < n / 2; i++)
+            {
+                if (estructura[i] != estructura[n - i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private bool BasesEnExtremos(List<char> estructura)
+        {
+            return estructura.Count > 1 && estructura[0] == '*' && estructura[^1] == '*';
+        }
+
+        public Puente(List<char> estructura)
+        {
+            _estructura = ValidarEstructura(estructura);
+        }
+    }
+}
